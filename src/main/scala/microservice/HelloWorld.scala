6f63c59fe1d500557e8e49e4d3b4fa69b5d3e7a3
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2020-07-02 08:30:10 +0200

    Update Scala, sbt, Play!, jsoniter-scala to the latest versions

--- src/main/scala/microservice/HelloWorld.scala
+++ src/main/scala/microservice/HelloWorld.scala
@@ -6,5 +6,5 @@ object HelloWorld {
   import com.github.plokhotnyuk.jsoniter_scala.core.JsonValueCodec
   import com.github.plokhotnyuk.jsoniter_scala.macros.{JsonCodecMaker, CodecMakerConfig}
 
-  implicit val helloWorldCodec: JsonValueCodec[HelloWorld] = JsonCodecMaker.make[HelloWorld](CodecMakerConfig())
+  implicit val helloWorldCodec: JsonValueCodec[HelloWorld] = JsonCodecMaker.make[HelloWorld]
 }
